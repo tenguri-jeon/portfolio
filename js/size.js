74b77799fbbd380f858618d1280cdcfdbc71a81b
tenguri-jeon <110875363+tenguri-jeon@users.noreply.github.com>
2023-03-02 19:12:05 +0900

    ball
    공클레스 주는거까지 했음 파편 사라지는거 하고,  opacity 0 , js로 클래스 줬다 빼는거 하면 됨

--- js/size.js
+++ js/size.js
@@ -1,16 +1,20 @@
-const $workoutOverflowContainer= document.getElementsByClassName('workout-overflow-container');
-let $workoutExplane = document.getElementsByClassName('work-out-explane');
-let workoutWidth;
-
-workoutSize();
-window.addEventListener('resize', ()=>{
-    workoutSize()
-    console.log(workoutWidth);
-});
-
-function workoutSize(){
-    workoutWidth = $workoutOverflowContainer[0].clientWidth; 
-    for (let i = 0; i < $workoutExplane.length; i++) {
-        $workoutExplane[i].style.width = `${workoutWidth}px`;
+sizeWorkout();
+function sizeWorkout() {
+    
+    const $workoutOverflowContainer= document.getElementsByClassName('workout-overflow-container');
+    let $workoutExplane = document.getElementsByClassName('work-out-explane');
+    let workoutWidth;
+    
+    workoutSize();
+    window.addEventListener('resize', ()=>{
+        workoutSize()
+    });
+    
+    function workoutSize(){
+        workoutWidth = $workoutOverflowContainer[0].clientWidth; 
+        for (let i = 0; i < $workoutExplane.length; i++) {
+            $workoutExplane[i].style.width = `${workoutWidth}px`;
+        }
     }
-}
+
+}        
--- js/workout.js
+++ js/workout.js
@@ -1,6 +1,8 @@
 const $workoutIndex = document.getElementsByClassName('workout-index');
 const $workOutExplane = document.getElementsByClassName('work-out-explane');
-const $folder = document.getElementsByClassName('folder');
+const $folder = document.querySelectorAll('.folder');
+const $workoutOverflowContainer = document.querySelector('.workout-overflow-container');
+const $workout = document.getElementsByClassName('goto-workout');
 
 
 for(let i = 0 ; i < $workoutIndex.length ; i++ ){
@@ -11,6 +13,15 @@ for(let i = 0 ; i < $workoutIndex.length ; i++ ){
     })
 }
 
+for (let i = 0; i < $workout.length; i++) {
+    $workout[i].addEventListener('click', ()=>{
+        resetWorkout();
+        $workout[i].classList.remove('back-index');
+        $workOutExplane[i].classList.add('workout-display')
+    })
+}
+
+
 function resetIndex(){
     for(let i = 0 ; i < $workoutIndex.length ; i ++){
         $workoutIndex[i].classList.remove('pick-index');
@@ -18,10 +29,21 @@ function resetIndex(){
     }
 }
 
-$folder.addEventListener('toggle' , ()=>{
-
-    for (let i = 0; i < $folder.length; i++) {
-        $folder[i].classList.add('folder-arrow-click');
+function resetWorkout() {
+    for (let i = 0; i < $workout.length; i++) {
+        $workout[i].classList.add('back-index');
+        $workOutExplane[i].classList.remove('workout-display');
     }
+}
 
-})
+folderText();
+function folderText(){
+    for (let i = 0; i < $folder.length; i++) {
+        $folder[i].addEventListener('click' , ()=>{
+            $folder[i].classList.toggle('folder-arrow-click');
+            $workoutOverflowContainer.classList.toggle('workout-height-unset');
+        }) 
+    }
+}
+
+
